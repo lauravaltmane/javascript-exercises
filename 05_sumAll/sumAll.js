c4d4f5c74a71643e25bfd7cd2711b6ca56b4191b
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 16:50:33 +0200

    Add initial logic for sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (startNumber, endNumber) {
+  let result = 0;
+  if (startNumber < 0 || endNumber < 0) {
+    return (result = "ERROR");
+  } else {
+    for (let i = startNumber; i <= endNumber; i++) {
+      result += i;
+    }
+  }
+  return result;
 };
+
+console.log(sumAll(1, 5));
 
 // Do not edit below this line
 module.exports = sumAll;
