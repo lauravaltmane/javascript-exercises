c3b61ee4a8ec91ab3edb7b775e05b956fab3897a
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 15:36:17 +0200

    Add function for reversing text strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (userInput) {
+  let userInputReversed = "";
+  for (let i = userInput.length - 1; i >= 0; i--) {
+    userInputReversed += userInput[i];
+  }
+  return userInputReversed;
 };
-
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
