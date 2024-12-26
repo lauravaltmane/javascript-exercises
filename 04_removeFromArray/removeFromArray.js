557d68fd37d3abc808a450c08621f8a192da848f
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 16:35:51 +0200

    Update function for filtering out array values to allow for multiple arguments and argument types

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,6 @@
-const removeFromArray = function (predefinedArray, elementToRemove) {
-  let finalArray = predefinedArray.filter((value) => value !== elementToRemove);
-  //console.log(finalArray);
-  return finalArray;
+const removeFromArray = function (predefinedArray, ...args) {
+  return predefinedArray.filter((val) => !args.includes(val));
 };
-console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
