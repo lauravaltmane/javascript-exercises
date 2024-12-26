d25cfd486c03e001783c1099f753daf7532f9f4f
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 17:25:14 +0200

    Add logic to check if input is an integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,33 @@
 const sumAll = function (startNumber, endNumber) {
   let result = 0;
-  if (startNumber < 0 || endNumber < 0) {
-    return (result = "ERROR");
+  let largestNumber = 0;
+  let smallestNumber = 0;
+  if (startNumber > endNumber) {
+    smallestNumber = endNumber;
+    largestNumber = startNumber;
   } else {
-    for (let i = startNumber; i <= endNumber; i++) {
+    largestNumber = endNumber;
+    smallestNumber = startNumber;
+  }
+  console.log(smallestNumber);
+  console.log(largestNumber);
+
+  if (
+    Number.isInteger(smallestNumber) &&
+    Number.isInteger(largestNumber) &&
+    smallestNumber >= 0 &&
+    largestNumber >= 0
+  ) {
+    for (let i = largestNumber; i >= smallestNumber; i--) {
       result += i;
     }
+  } else {
+    result = "ERROR";
   }
   return result;
 };
 
-console.log(sumAll(1, 5));
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
