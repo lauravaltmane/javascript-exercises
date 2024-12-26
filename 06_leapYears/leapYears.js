3afbc363a2561cc2a5e866456325a5c725702f76
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 17:53:13 +0200

    Add logic for calculating if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+//> Leap years are years divisible by four (like 1984 and 2004).
+// However, years divisible by 100 are not leap years (such as 1800 and 1900)
+// unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
 
+const leapYears = function (year) {
+  let result = false;
+  if (year % 4 == 0) {
+    result = true;
+  }
+  if (year % 100 == 0) {
+    result = false;
+  }
+  if (year % 400 == 0) {
+    result = true;
+  }
+  return result;
 };
-
+console.log(leapYears(2000)); // is a leap year: returns true
+console.log(leapYears(1800)); // is a leap year: returns false
+console.log(leapYears(1985)); // is not a leap year: returns false
 // Do not edit below this line
 module.exports = leapYears;
