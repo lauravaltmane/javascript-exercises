21a1a55e34615b39e043ac6929b37e1e06681401
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 14:58:03 +0200

    Add function and define variables for repeating string a set number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+let result = "";
 
+const repeatString = function (string, num) {
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
 };
+repeatString("hey", 3);
 
+console.log(result);
 // Do not edit below this line
 module.exports = repeatString;
