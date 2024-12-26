c5619318d0eefad3885de8e1a3409e107e6fe33a
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 16:06:59 +0200

    Add function for filtering out a specific value from an array and returning the filtered array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (predefinedArray, elementToRemove) {
+  let finalArray = predefinedArray.filter((value) => value !== elementToRemove);
+  //console.log(finalArray);
+  return finalArray;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
