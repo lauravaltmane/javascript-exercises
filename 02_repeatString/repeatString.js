bdadc4486da2b6bafe727a86f14de310d75d66f5
lauravaltmane <lauraavmail@gmail.com>
2024-12-26 15:15:49 +0200

    Changes tu ensure function returns value when num argument is positive and error when num argument value is negative.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,15 @@
-let result = "";
-
 const repeatString = function (string, num) {
-  for (let i = 0; i < num; i++) {
-    result += string;
+  let result = "";
+  if (num < 0) {
+    result = "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      result += string;
+    }
   }
+  console.log(result);
+  return result;
 };
-repeatString("hey", 3);
-
-console.log(result);
+repeatString("hey", -3);
 // Do not edit below this line
 module.exports = repeatString;
